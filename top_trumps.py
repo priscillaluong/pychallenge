3cb0d0c27ab026e9a5a65b9b07e1f5850fa22839
Priscille <p-luong@live.co.uk>
2020-05-24 13:27:53 +0100

    Update top_trumps.py

--- top_trumps.py
+++ top_trumps.py
@@ -1,1 +1,11 @@
 # Top Trumps
+
+# A small game where players compare stats. The basic flow of the game is:
+# 1. You are given a random card with different stats.
+# 2. You select one of the card's stats.
+# 3. Another random card is selected for your opponent (the computer)
+# 4. The stats of the two cards are compared.
+# 5. The player with the stat higher than their opponent wins. 
+
+# The project will use the Pokemon API
+
