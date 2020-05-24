068773109dcc9264c61b4e3e887a0b31f891ff20
Priscille <p-luong@live.co.uk>
2020-05-24 16:45:35 +0100

    Update top_trumps.py

--- top_trumps.py
+++ top_trumps.py
@@ -9,30 +9,43 @@
 
 # The project will use the Pokemon API
 
-# Generate a random number between 1 and 151 to use as the Pokemon ID number
+def random_pokemon():
+    # Generate a random number between 1 and 151 to use as the Pokemon ID number
+    import random
+    pokemon_number = random.randint(1, 152)
+    import requests
+    import json
 
-import random
-pokemon_number = random.randint(1, 152)
+    # Using the Pokemon API get a Pokemon based on its ID number
+    url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
+    response = requests.get(url)
+    pokemon = response.json()
+    # print(pokemon['name'].title() + '\n' + str(pokemon_number) + '\n' + str(pokemon['height']) + '\n' + str(pokemon['weight']))
 
-# Using the Pokemon API get a Pokemon based on its ID number
+    # Create a dictionary that contains the returned Pokemon's name id, height and weight
+    return {
+        'name':pokemon['name'],
+        'id':pokemon['id'],
+        'height':pokemon['height'],
+        'weight':pokemon['weight']
+    }
 
-import requests
-import json
+# Get a random Pokemon for the player and another for their opponent
+def get():
+    my_pokemon = random_pokemon()
+    print("Your pokemon is {} with ID: {}, height: {}, and weight: {}.".format(my_pokemon['name'].title(), my_pokemon['id'], my_pokemon['height'], my_pokemon['weight']))
 
-url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
-response = requests.get(url)
-pokemon = response.json()
-poke_name = pokemon['name']
-print(poke_name.title() + '\n' + str(pokemon_number))
+    opponents_pokemon = random_pokemon()
+    print("The opponent's pokemon is: {}".format(opponents_pokemon['name'].title()))
 
-# Create a dictionary that contains the returned Pokemon's name id, height and weight
+    # Ask the player which stat they want to use
+    chosen_stat = input('Which stat do you want to use? ')
 
-pokemon_dict = {
-    'name':poke_name.title(),
-    'id':pokemon_number,
-    'height':pokemon['height'],
-    'weight':pokemon['weight']
-}
-
-print(pokemon_dict)
-
+    # Compare the opponents and players stat
+    if my_pokemon[chosen_stat] > opponents_pokemon[chosen_stat]:
+        print('You win!')
+    elif my_pokemon[chosen_stat] < opponents_pokemon[chosen_stat]:
+        print('You lose!')
+    elif my_pokemon[chosen_stat] == opponents_pokemon[chosen_stat]:
+        print('You draw!')
+get()
